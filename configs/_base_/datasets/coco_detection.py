a03c3095f4ef8294a5605a1b8d0c2c24761c7e3b
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2023-01-11 20:35:44 +0800

    Update coco_detection.py

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '../input/coco-style/coco_style/'
+data_root = '/content/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -29,21 +29,28 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=8,
+    workers_per_gpu=8,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/train.json',
-        img_prefix=data_root + 'images/train/',
+        ann_file=data_root + 'drive/MyDrive/ssdd_coco/train1/train.json',
+        #ann_file=data_root + 'gdrive/MyDrive/HRSID_JPG/annotations/train2017.json',
+        #img_prefix=data_root + 'gdrive/MyDrive/HRSID_JPG/train_image/',
+        img_prefix=data_root + 'drive/MyDrive/ssdd_coco/train/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/test.json',
-        img_prefix=data_root + 'images/test/',
+        #ann_file=data_root + 'gdrive/MyDrive/HRSID_JPG/annotations/test2017.json',
+        #img_prefix=data_root + 'gdrive/MyDrive/HRSID_JPG/test_image/',
+        img_prefix=data_root + 'drive/MyDrive/ssdd_coco/val/',
+        ann_file=data_root +'drive/MyDrive/ssdd_coco/val1/val.json',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/test.json',
-        img_prefix=data_root + 'images/test/',
+        #ann_file=data_root + 'gdrive/MyDrive/HRSID_JPG/annotations/test2017.json',
+        ann_file=data_root + 'drive/MyDrive/ssdd_coco/val1/val.json',
+        #img_prefix=data_root + 'drive/MyDrive/HRSID_JPG/test_image/',
+        img_prefix=data_root + 'drive/MyDrive/ssdd_coco/val/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
+
